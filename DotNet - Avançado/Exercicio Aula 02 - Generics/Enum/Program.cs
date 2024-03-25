06ca74d44d9f82daeeef2613668eff9401d501b2
Wilton Freire <wilton.lio@gmail.com>
2024-03-25 12:00:54 -0300

    Enviando Exercicio 03

--- DotNet - Avançado/Exercicio Aula 02 - Generics/Enum/Program.cs
+++ DotNet - Avançado/Exercicio Aula 02 - Generics/Enum/Program.cs
@@ -1,2 +1,52 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Exercicio
+{
+    enum Exercicio
+    {
+        Academia = 1,
+        Luta = 2,
+        Corrida = 3,
+        Sair = 4
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercícios:");
+            foreach (Exercicio exercicio in Enum.GetValues(typeof(Exercicio)))
+            {
+                Console.WriteLine($"{Convert.ToInt32(exercicio)} - {exercicio}");
+            }
+
+            Console.Write("Escolha um exercício: ");
+            int opcaoEscolhida = int.Parse(Console.ReadLine());
+
+            Exercicio exercicioEscolhido = (Exercicio)opcaoEscolhida;
+
+            Console.WriteLine($"Você escolheu: {exercicioEscolhido}");
+
+            while (exercicioEscolhido != Exercicio.Sair)
+            {
+                Console.WriteLine("Escolha um exercício:");
+                foreach (Exercicio exercicio in Enum.GetValues(typeof(Exercicio)))
+                {
+                    Console.WriteLine($"{Convert.ToInt32(exercicio)} - {exercicio}");
+                }
+
+                Console.Write("Escolha um exercício: ");
+                opcaoEscolhida = int.Parse(Console.ReadLine());
+
+                exercicioEscolhido = (Exercicio)opcaoEscolhida;
+
+                Console.WriteLine($"Você escolheu: {exercicioEscolhido}");
+            }
+
+
+        }
+    }
+}
+
+
+
