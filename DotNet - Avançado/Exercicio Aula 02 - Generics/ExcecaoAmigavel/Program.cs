26330b0547d8e95d25f9857a7476df251f22714e
Wilton Freire <wilton.lio@gmail.com>
2024-03-25 11:49:17 -0300

    Enviando exercício 02

---
+++ DotNet - Avançado/Exercicio Aula 02 - Generics/ExcecaoAmigavel/Program.cs
@@ -0,0 +1,12 @@
+﻿object o = null;
+
+try
+{
+    o.ToString();
+}
+catch (NullReferenceException ex)
+{
+    throw new Exception("Não é possível chamar o método ToString() em um objeto nulo. Verifique se o objeto é válido antes de chamar este método.", ex);
+}
+
+//Tipo de exceção: Unhandled exception. System.NullReferenceException: Object reference not set to an instance of an object.
