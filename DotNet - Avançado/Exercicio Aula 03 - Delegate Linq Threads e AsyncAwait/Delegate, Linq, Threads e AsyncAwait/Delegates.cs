2b42fe82ffa91b9c652916a94464f5f236db3299
Wilton Freire <wilton.lio@gmail.com>
2024-04-08 10:10:24 -0300

    enviando delagate

---
+++ DotNet - Avançado/Exercicio Aula 03 - Delegate Linq Threads e AsyncAwait/Delegate, Linq, Threads e AsyncAwait/Delegates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> lista = new List<double> { 3, 7, 2, 4, 6 };
+
+            List<double> novaLista = lista.ConvertAll(elemento => elemento / 2);
+
+            novaLista.ForEach(elemento => Console.WriteLine(elemento));
+        }
+    }
+}
+
