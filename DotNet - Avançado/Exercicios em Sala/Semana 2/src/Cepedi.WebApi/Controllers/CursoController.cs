d05e523ee6d3933d05a1fcabf08d5501e8037f7b
WiltonLio <wilton.lio@gmail.com>
2024-04-10 14:23:26 -0300

    Enviando arquivos

---
+++ DotNet - Avançado/Exercicios em Sala/Semana 2/src/Cepedi.WebApi/Controllers/CursoController.cs
@@ -0,0 +1,25 @@
+﻿using Cepedi.Data.Repositories.Interfaces;
+using Cepedi.Shareable.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cepedi.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CursoController : ControllerBase
+{
+    private readonly ILogger<CursoController> _logger;
+    private readonly ICursoRepository _cursoRepository;
+
+    public CursoController(ILogger<CursoController> logger, ICursoRepository cursoRepository)
+    {
+        _logger = logger;
+        _cursoRepository = cursoRepository;
+    }
+
+    [HttpGet("{idCurso}")]
+    public async Task<ActionResult<ObtemCursoResponse>> ConsultarCursoAsync([FromRoute] int idCurso)
+    {
+        return Ok();
+    }
+}
